4959e14759e68b45275a54bfffb4661d4ee39604
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2023-07-12 18:19:38 +0200

    updated build files

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hyperion-dev/",
   test: {
     environment: "happy-dom",
     coverage: {
